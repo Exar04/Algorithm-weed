23a0260a687536f8432a2cbb08f392858a5e52fc
Yash dhadve <yashdhadwe@gmail.com>
2023-05-07 01:28:09 +0530

    made it worse

--- main.go
+++ main.go
@@ -10,9 +10,13 @@ func multiply(n int) {
 } 
 
 func main() {
+    slowItMore := 0 
+    fmt.Print("Enter a number to make it more slow - ")
+    fmt.Scanf("%d", &slowItMore)
+    fmt.Println()
     count := 1
-    for i:= 10000000000; i < 100000000000000000 ; i++{
-        multiply(i)
+    for i:= 10000000000 ; i < 100000000000000000 ; i++{
+        multiply(i* slowItMore)
         fmt.Println(count)
         count++
     }
